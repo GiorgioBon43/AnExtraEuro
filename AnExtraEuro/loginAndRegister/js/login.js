a71f0969563c9e0f6b1771f4b1cccdcac4f20daf
Giorgiobon <101709335+Giorgiobon@users.noreply.github.com>
2024-01-11 19:06:15 +0100

    Add files via upload

--- AnExtraEuro/loginAndRegister/js/login.js
+++ AnExtraEuro/loginAndRegister/js/login.js
@@ -1,15 +1,23 @@
 document.querySelector('form').addEventListener('submit', async function (e) {
-	e.preventDefault();
+    e.preventDefault();
     const username = document.querySelector("input[name='username']").value;
-	const email = document.querySelector("input[name='email']").value;
-	const password = document.querySelector("input[name='password']").value;
+    const email = document.querySelector("input[name='email']").value;
+    const password = document.querySelector("input[name='password']").value;
 
     console.log(username, email, password);
 
-    const res = await fetch('/login/log', {
-		method: 'GET',
-	}).then(() => {
-		window.location.href = '/login/log';
-	});
+    try {
+        const res = await fetch('/login/log', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, email, password }),
+        }).then(() => {
+			// Dopo la richiesta, reindirizza l'utente a /login
+			window.location.href = '/login/log';
+		});
+    } catch (error) {
+        console.error('Errore durante la richiesta:', error);
+    }
 });
-

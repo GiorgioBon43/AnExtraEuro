964fc07e0cc57d4a32bedd9282174ed766901498
Giorgiobon <101709335+Giorgiobon@users.noreply.github.com>
2024-02-05 13:44:38 +0100

    Update scriptINSERTdata.sql

--- AnExtraEuro/dataBase config/scriptINSERTdata.sql
+++ AnExtraEuro/dataBase config/scriptINSERTdata.sql
@@ -1,29 +1,38 @@
 INSERT INTO ACCOUNT (NICKNAME, EMAIL, PASSWORD, ABILITATO)
 VALUES 
-    ('user1', 'user1@example.com', 'password1', FALSE),
-    ('user2', 'user2@example.com', 'password2', TRUE),
-    ('user3', 'user3@example.com', 'password3', FALSE),
-    ('user4', 'user4@example.com', 'password4', FALSE),
-    ('user5', 'user5@example.com', 'password5', FALSE);
+    ('gio', 'gio@io.com', 'io', TRUE),
+    ('paolo', 'paolo@io.com', 'tu', FALSE),
+    ('aronne', 'aronne@io.com', 'egli', FALSE);
 
 INSERT INTO CATEGORIA (NOMINATIVO, DESCRIZIONE)
 VALUES 
-    ('Category1', 'Description for Category1'),
-    ('Category2', 'Description for Category2'),
-    ('Category3', 'Description for Category3');
+    ('Fumetto', 'Libro con vignete'),
+    ('Video Gioco', 'Gioco digitale'),
+    ('Gioco da tavolo', 'Gioco cartaceo');
 
 INSERT INTO PROGETTO (NOME, DESCRIZIONE, ACCOUNT_NICKNAME, CATEGORIA_NOMINATIVO, OBBIETTIVO)
 VALUES 
-    ('Project1', 'Description for Project1', 'user1', 'Category1', 1000),
-    ('Project2', 'Description for Project2', 'user2', 'Category2', 2000),
-    ('Project3', 'Description for Project3', 'user3', 'Category3', 3000),
-    ('Project4', 'Description for Project4', 'user4', 'Category1', 4000),
-    ('Project5', 'Description for Project5', 'user5', 'Category2', 5000);
+    ('Progetto1', 'Progetto1', 'gio', 'Fumetto', 1000.00),
+    ('Progetto2', 'Progetto2', 'paolo', 'Video Gioco', 2000.00),
+    ('Progetto3', 'Progetto3', 'aronne', 'Gioco da tavolo', 3000.00),
+    ('Progetto4', 'Progetto4', 'gio', 'Fumetto', 4000.00),
+    ('Progetto5', 'Progetto5', 'paolo', 'Video Gioco', 5000.00);
 
 INSERT INTO DONARE (ACCOUNT_NICKNAME, PROGETTO_ID, VALORE_DONAZIONE)
 VALUES 
-    ('user1', 1, 100.50),
-    ('user2', 2, 200.75),
-    ('user3', 3, 300.25),
-    ('user4', 4, 400.00),
-    ('user5', 5, 500.50);
+    ('gio', 1, 100.00),
+    ('paolo', 1, 200.00),
+    ('aronne', 1, 300.00),
+    ('gio', 2, 400.00),
+    ('paolo', 2, 500.00),
+    ('aronne', 2, 600.00),
+    ('gio', 3, 700.00),
+    ('paolo', 3, 800.00),
+    ('aronne', 3, 900.00),
+    ('gio', 4, 1000.00),
+    ('paolo', 4, 1100.00),
+    ('aronne', 4, 1200.00),
+    ('gio', 5, 1300.00),
+    ('paolo', 5, 1400.00),
+    ('aronne', 5, 1500.00);
+```

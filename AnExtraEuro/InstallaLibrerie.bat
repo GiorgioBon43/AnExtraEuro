5715c8a5f884d53e5a9c400bb244455e447e2922
Giorgiobon <101709335+Giorgiobon@users.noreply.github.com>
2024-04-15 12:59:17 +0200

    Add files via upload

---
+++ AnExtraEuro/InstallaLibrerie.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem Install Express
+npm install express --save
+
+rem Install Express Sessions
+npm install express-session --save
+
+rem Install JWT
+npm install jsonwebtoken
+
+rem Install MySQL for Node.js
+npm install mysql --save
+
+rem Install React latest version
+npm i react@latest react-dom@latest
